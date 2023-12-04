c136761837e99d4d8035d7a742cb46efa1017438
es-mitty <esmitty1212@gmail.com>
2023-12-03 22:57:49 -0500

    program to print the temperature

--- main/temp_sensor.h
+++ main/temp_sensor.h
@@ -1,6 +1,8 @@
 #include "onewire_bus.h"
 #include "ds18b20.h"
 
+void print_temp(ds18b20_device_handle_t device_handle);
+
 esp_err_t get_single_temp_sensor(int gpio_pin, ds18b20_device_handle_t *device_handle){
     // Onewire variables
     onewire_bus_config_t bus_config = {gpio_pin};
@@ -12,15 +14,21 @@ esp_err_t get_single_temp_sensor(int gpio_pin, ds18b20_device_handle_t *device_h
     ds18b20_config_t sensor_config = {};
 
     // Init bus
-    ESP_ERROR_CHECK(onewire_new_bus_rmt(&bus_config, &rmt_config, bus_handle));
+    ESP_ERROR_CHECK(onewire_new_bus_rmt(&bus_config, &rmt_config, &bus_handle));
     // Get first device
-    ESP_ERROR_CHECK(onewire_new_device_iter(&bus_handle, &device_iter));
-    ESP_ERROR_CHECK(onewire_device_iter_get_next(&device_iter, &device));
+    ESP_ERROR_CHECK(onewire_new_device_iter(bus_handle, &device_iter));
+    ESP_ERROR_CHECK(onewire_device_iter_get_next(device_iter, &device));
     // Convert to DS18B20
     ESP_ERROR_CHECK(ds18b20_new_device(&device, &sensor_config, device_handle));
     // Print a temp
-    ESP_ERROR_CHECK(ds18b20_trigger_temperature_conversion(device_handle));
+    print_temp(*device_handle);
+
+    return ESP_OK;
+}
+
+void print_temp(ds18b20_device_handle_t device_handle){
+    ds18b20_trigger_temperature_conversion(device_handle);
     float temp = 0;
-    ESP_ERROR_CHECK(ds18b20_get_temperature(device_handle, &temp));
-    printf("Temperature is %.2f degrees.", temp);
+    ds18b20_get_temperature(device_handle, &temp);
+    printf("Temperature is %.5f degrees.\n", temp);
 }
--- main/thermostat_client.c
+++ main/thermostat_client.c
@@ -4,7 +4,16 @@
 #include "temp_sensor.h"
 #include "mdns_tasks.h"
 
+#define TEMP_SENSOR_PIN 13
+
 void app_main(void)
 {
+    ds18b20_device_handle_t handle;
+    get_single_temp_sensor(TEMP_SENSOR_PIN, &handle);
+
+    while(1){
+        print_temp(handle);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 
 }
