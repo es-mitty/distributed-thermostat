db63e3150a79f2d077e70f650810a0f811fb6489
es-mitty <esmitty1212@gmail.com>
2023-12-03 22:31:50 -0500

    init variables

--- main/temp_sensor.h
+++ main/temp_sensor.h
@@ -1,6 +1,19 @@
 #include "onewire_bus.h"
 #include "ds18b20.h"
 
-esp_err_t get_single_onewire(int gpioPin){
-    
+esp_err_t get_single_temp_sensor(int gpio_pin, ds18b20_device_handle_t *device_handle){
+    // Onewire variables
+    onewire_bus_config_t bus_config = {gpio_pin};
+    onewire_bus_rmt_config_t rmt_config = {10};
+    onewire_bus_handle_t bus_handle;
+    onewire_device_iter_handle_t device_iter;
+    onewire_device_t device;
+    // DS18B20 variables
+    ds18b20_config_t sensor_config = {};
+    ds18b20_device_handle_t temp_sensor;
+
+    // Init bus
+    // Get first device
+    // Convert to DS18B20
+    // Print a temp
 }
