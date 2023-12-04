485f79c9a089554b65fd8f0a5c0989e6311b9627
es-mitty <esmitty1212@gmail.com>
2023-12-04 08:07:13 -0500

    Working threaded task

--- main/thermostat_client.c
+++ main/thermostat_client.c
@@ -8,13 +8,22 @@
 
 ds18b20_device_handle_t *temp_sensor;
 
-void app_main(void)
-{
-    temp_sensor = malloc(sizeof(ds18b20_device_handle_t));
-    get_single_temp_sensor(TEMP_SENSOR_PIN, temp_sensor);
+void temp_task(void *args){
+    TempSensoryMemory *sensor_mem = (TempSensoryMemory*)(args);
+
+    sensor_mem->bus_config.bus_gpio_num = TEMP_SENSOR_PIN;
+    sensor_mem->rmt_config.max_rx_bytes = 10;
+
+    get_single_temp_sensor(TEMP_SENSOR_PIN, sensor_mem);
 
     while(1){
-        print_temp(*temp_sensor);
+        print_temp(sensor_mem->temp_sensor_handle);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+}
+
+void app_main(void)
+{
+    TempSensoryMemory *temp_sensor_mem = malloc(sizeof(TempSensoryMemory));
+    xTaskCreate(&temp_task, "Temp", 4096, temp_sensor_mem, 1, NULL);
 }
