22276209a04afd83ddbd388a035523afa1110421
es-mitty <esmitty1212@gmail.com>
2023-12-06 21:35:56 -0500

    first http server version testing

---
+++ main/http_server.h
@@ -0,0 +1,34 @@
+#include "esp_http_server.h"
+
+// Basic Handle that returns "OK"
+esp_err_t basic_handler(httpd_req_t* req){
+    
+    const char* resp = "This is OK!";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    
+    return ESP_OK;
+}
+httpd_uri_t base_uri{
+    .uri = "/",
+    .method = HTTPD_GET,
+    .handler = basic_handler,
+    .user_ctx = NULL
+};
+
+// Server starter
+httpd_handle_t start_webserver(void)
+{
+    /* Generate default configuration */
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    /* Empty handle to esp_http_server */
+    httpd_handle_t server = NULL;
+
+    /* Start the httpd server */
+    if (httpd_start(&server, &config) == ESP_OK) {
+        /* Register URI handlers */
+        httpd_register_uri_handler(server, &base_uri);
+    }
+    /* If server failed to start, handle will be NULL */
+    return server;
+}
--- main/thermostat_client.c
+++ main/thermostat_client.c
@@ -3,8 +3,13 @@
 #include "freertos/task.h"
 #include "temp_sensor.h"
 #include "mdns_tasks.h"
+#include "http_server.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
 #define TEMP_SENSOR_PIN 13
+#define SSID "NARP House"
+#define PASSWORD "slant3dR00f"
 
 ds18b20_device_handle_t *temp_sensor;
 
@@ -24,6 +29,29 @@ void temp_task(void *args){
 
 void app_main(void)
 {
+
+    static httpd_handle_t server = NULL;
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t winit_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&winit_config));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = SSID,
+            .password = PASSWORD
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
     TempSensoryMemory *temp_sensor_mem = malloc(sizeof(TempSensoryMemory));
     xTaskCreate(&temp_task, "Temp", 4096, temp_sensor_mem, 1, NULL);
+
+    server = start_webserver();
 }
