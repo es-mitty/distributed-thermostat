837b33cc1e2c64c6b96d1d11ca8f6c1c23c5ed2d
es-mitty <esmitty1212@gmail.com>
2023-12-08 08:32:34 -0500

    data structures for temperature data

---
+++ main/temp_data.h
@@ -0,0 +1,12 @@
+
+typedef struct thermo_data_t{
+    char node_name[30];
+    float target_temp;
+    float measured_temp;
+};
+
+typedef struct thermo_sys_t{
+    char system_name[30];
+    int num_sensors;
+    thermo_data_t *zones;
+};
